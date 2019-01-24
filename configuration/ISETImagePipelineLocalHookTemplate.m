cad3f3ade8008f49750735075379afe9fed6e655
David Brainard <brainard@psych.upenn.edu>
2019-01-24 11:04:22 -0500

    Add config

---
+++ configuration/ISETImagePipelineLocalHookTemplate.m
@@ -0,0 +1,50 @@
+% ISETImagePipelineLocalHookTemplate
+%
+% Template for setting preferences and other configuration things, for the
+% ISETImagePipeline project.
+
+%% Define project
+toolboxName = 'ISETImagePipeline';
+
+%% Clear out old preferences
+if (ispref(toolboxName))
+    rmpref(toolboxName);
+end
+
+%% Specify project location
+istsBaseDir = tbLocateProject('ISETImagePipeline');
+
+% Figure out where baseDir for other kinds of data files is.
+%
+% Can only do this when we have GetComputerInfo available.
+if (exist('GetComputerInfo','file'))
+    sysInfo = GetComputerInfo();
+    switch (sysInfo.localHostName)
+        case 'eagleray'
+            % DHB's desktop
+            baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
+            
+        case {'Manta', 'Manta-2'}
+            % Nicolas's iMac
+            baseDir = fullfile(filesep,'Volumes','DropBoxDisk/Dropbox','Dropbox (Aguirre-Brainard Lab)');
+            
+        otherwise
+            % Some unspecified machine, try user specific customization
+            switch(sysInfo.userShortName)
+                % Could put user specific things in, but at the moment generic
+                % is good enough.
+                otherwise
+                    baseDir = ['/Users/' sysInfo.userShortName '/Dropbox (Aguirre-Brainard Lab)'];
+            end
+    end
+end
+
+% Full path to data
+%   Get path to data in project code with getpref('ISETImagePipeline','dataDir');
+setpref(toolboxName,'dataDir',fullfile(baseDir,'IBIO_Analysis','ISETImagePipeline'));
+
+
+
+
+
+
