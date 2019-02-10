f3355c215c7b86a7a198f07af982e8252cd80550
zlqzcc <lz1569@nyu.edu>
2019-02-09 20:25:19 -0500

    isetbio approximation

--- compute/iset_approximation.m
+++ compute/iset_approximation.m
@@ -24,12 +24,70 @@ imageTe = D{1}; clear D;
 
 nTrain = size(imageTr, 1);
 nTest  = size(imageTe, 1);
+nDiag  = min(size(imageTr));
+
+%% Tricks
+imageTr = [imageTr, ones(25000, 1)];
+imageTe = [imageTe, ones(5000, 1)];
 
 %% Learning the regression estimator
-nDiag = min(size(coneVecTr));
 regEstimator = RegressionEstimator(imageTr, coneVecTr);
 
-%% Full evaluation
+%% Simple evaluation
 evalObj = CrossValidation(imageTe, coneVecTe, nTest);
+[recon, gt] = evalObj.sampleTest(regEstimator, false);
+
+figure;
+scatter(gt, recon); grid on;
+xlabel('True Cone Mean Response');
+ylabel('Estimated Cone Mean Response');
+axis equal;
+axis square;
+
+%% Full evaluation
 [totalMSE, listMSE] = evalObj.evalTest(regEstimator);
 
+%% Cross validation
+stepSize = round(nDiag / 20);
+regEstimator.setParaList(stepSize : stepSize : nDiag);
+[paraList, mse] = evalObj.crossValidate(regEstimator);
+
+%% Reset regularization parameter
+regEstimator.setRegPara(600);
+
+%% Simple evaluation
+nSample = 36;
+for idx = 1:nSample
+    subplot(6, 6, idx);
+    [recon, gt] = evalObj.sampleTest(regEstimator, false);
+
+
+    scatter(gt, recon); grid on; hold on;
+    refPoint = [-500, 5000];
+    plot(refPoint, refPoint);
+    
+    axis square;    
+    xlim(refPoint);
+    ylim(refPoint);
+end
+
+%% Learning the normalized regression estimator
+nrmregEstimator = NrmRegressionEstimator(imageTr, coneVecTr);
+
+%% Simple evaluation
+evalObj = CrossValidation(imageTe, coneVecTe, nTest);
+
+nSample = 36;
+for idx = 1:nSample
+    subplot(6, 6, idx);
+    [recon, gt] = evalObj.sampleTest(nrmregEstimator, false);
+
+    scatter(gt, recon); grid on; hold on;
+    refPoint = [-500, 5000];
+    plot(refPoint, refPoint);
+    xlabel('True Cone Mean Response');
+    ylabel('Estimated Cone Mean Response');    
+    axis square;    
+    xlim(refPoint);
+    ylim(refPoint);
+end
