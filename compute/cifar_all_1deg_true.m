bd601250e2676d203758a466e005585f880b151d
zlqzcc <lz1569@nyu.edu>
2019-02-21 16:03:34 -0500

    update pipeline

--- compute/cifar_all_1deg_true.m
+++ compute/cifar_all_1deg_true.m
@@ -1,6 +1,6 @@
 projectName = 'ISETImagePipeline';
 thisImageSet = 'CIFAR_all';
-theDir = 'all_1_true_coneVec';
+theDir = 'all_1_true_dataset';
 dataBaseDir = getpref(projectName, 'dataDir');
 dataFileIn = fullfile(dataBaseDir, thisImageSet, 'image_cifar_all.mat');
 dataDirOut = fullfile(dataBaseDir, thisImageSet, theDir);
@@ -9,22 +9,31 @@ if (~exist(dataDirOut, 'dir'))
 end
 
 load(dataFileIn);
-retina = ConeResponse('eccBasedConeDensity', true, 'eccBasedConeQuantal', true);
+displayFile = 'CRT12BitDisplay.mat';
+display = load(fullfile(dataBaseDir, displayFile));
+
+retina = ConeResponse('eccBasedConeDensity', true, 'eccBasedConeQuantal', true, ...
+                      'display', display.CRT12BitDisplay);
 
 testImage = reshape(image_all(1, :), [32, 32, 3]);
-[~, ~, testConeVec] = retina.compute(testImage);
+[~, ~, testLinearImage, testConeVec, ~, ~, ~] = retina.compute(testImage);
 
 nImage = 1e5;
 allConeVec = zeros(nImage, length(testConeVec));
+allLinearImage = zeros(nImage, length(testLinearImage(:)));
 
-parfor idx = 1 : nImage
+parfor idx = 1:nImage
     
     inputImage = reshape(image_all(idx, :), [32, 32, 3]);    
-    [~, ~, coneExcitation] = retina.compute(inputImage);
+    [~, ~, linearImage, coneExcitation, ~, ~, ~] = retina.compute(inputImage);
     
     allConeVec(idx, :) = coneExcitation;
+    allLinearImage(idx, :) = linearImage(:);
     
 end
+
+linearImageFile = 'linearImage100k.mat';
+save(fullfile(dataDirOut, linearImageFile), 'allLinearImage', '-v7.3');
 
 coneVectorFile = 'coneVector100k.mat';
 save(fullfile(dataDirOut, coneVectorFile), 'allConeVec', '-v7.3');
