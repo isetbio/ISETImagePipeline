815f65714bf40e5147cd627e27ec39019c7675e1
zlqzcc <lz1569@nyu.edu>
2019-02-28 12:58:16 -0500

    fix bug

---
+++ tutorials/t_gaussianPCA.m
@@ -0,0 +1,80 @@
+%% Setup training and test dataset
+projectName = 'ISETImagePipeline';
+thisImageSet = 'CIFAR_all';
+dataBaseDir = getpref(projectName, 'dataDir');
+dataInDir = fullfile(dataBaseDir, thisImageSet, 'all_1_true_dataset');
+
+coneVecTr  = 'coneVector20k.mat';
+coneVecTe  = 'coneVector20k.mat';
+
+idxTrain = 1:1:1.8e4;
+idxTest  = (2e4 - 2e3 + 1):1:2e4;
+
+D = struct2cell(load(fullfile(dataInDir, coneVecTr)));
+coneVecTr = D{1}; coneVecTr = coneVecTr(idxTrain, :); clear D;
+D = struct2cell(load(fullfile(dataInDir, coneVecTe)));
+coneVecTe = D{1}; coneVecTe = coneVecTe(idxTest, :); clear D;
+
+imageTr = 'linearImage20k.mat';
+imageTe = 'linearImage20k.mat';
+
+D = struct2cell(load(fullfile(dataInDir, imageTr)));
+imageTr = D{1}; imageTr = imageTr(idxTrain, :); clear D;
+D = struct2cell(load(fullfile(dataInDir, imageTe)));
+imageTe = D{1}; imageTe = imageTe(idxTest, :); clear D;
+
+nTrain = size(imageTr, 1);
+nTest  = size(imageTe, 1);
+nDiag  = min(size(imageTr));
+
+%% Estimate the render matrix
+% We want to use a linear approximation of the isetbio routine (which is
+% linear indeed up to cone isomerization. The next few sections evaluate
+% how good the linear estimation is (should be perfect).
+
+regEstimator = RegressionEstimator(imageTr, coneVecTr);
+
+%% Simple evaluation
+evalObj = CrossValidation(imageTe, coneVecTe, nTest);
+nSample = 36;
+for idx = 1:nSample
+    subplot(6, 6, idx);
+    [recon, gt] = evalObj.sampleTest(regEstimator, false);
+    
+    scatter(gt, recon); grid on; hold on;
+    refPoint = [-500, 4000];
+    plot(refPoint, refPoint);
+    
+    axis square;    
+    xlim(refPoint);
+    ylim(refPoint);
+    
+    if(idx == 34)
+        xlabel('True Cone Mean Response');
+    end
+    
+    if(idx == 13)
+        ylabel('Estimated Cone Mean Response');
+    end    
+end
+
+%% Full evaluation
+[totalMSE, listMSE] = evalObj.evalTest(regEstimator);
+
+listMSE = sort(listMSE);
+histogram(listMSE(1:floor(nTest * 0.975)));
+
+%% Learn PCA basis
+% We want to learn a PCA basis for image reconstruction, which should be
+% equivalent to Gaussian markov image model, or 1/f statistics in the
+% frequency domain.
+
+[regBasis, mu] = computeBasisPCA(imageTr, 32);
+
+%% Gaussian Prior - Poisson Likelihood estimator
+renderMatrix = regEstimator.W';
+estimator = PoissonGaussianEst(renderMatrix, regBasis, mu');
+
+%% Simple evaluation
+evalObj = CrossValidation(coneVecTe, imageTe, nTest);
+[recon, gt] = evalObj.sampleTest(estimator, false);
