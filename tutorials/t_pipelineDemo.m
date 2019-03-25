42e6a84b286470f5317d416be29e0b832a6df966
lingqiz <zlqzcc@gmail.com>
2019-03-25 11:11:34 -0400

    update demo code

--- tutorials/t_pipelineDemo.m
+++ tutorials/t_pipelineDemo.m
@@ -1,32 +1,31 @@
-%% Generate dataset for learning render matrix
+%% Load dataset, create the cone mosaic
 projectName = 'ISETImagePipeline';
 thisImageSet = 'CIFAR_all';
-theDir = 'all_0.125_true_dataset';
 dataBaseDir = getpref(projectName, 'dataDir');
 dataFileIn = fullfile(dataBaseDir, thisImageSet, 'image_cifar_all.mat');
-dataDirOut = fullfile(dataBaseDir, thisImageSet, theDir);
-if (~exist(dataDirOut, 'dir'))
-    mkdir(dataDirOut);
-end
 
 load(dataFileIn);
 displayFile = 'CRT12BitDisplay.mat';
 display = load(fullfile(dataBaseDir, displayFile));
 
+
+% retina = ConeResponse('spatialDensity', [0, 0.6, 0.3, 0.1 ], 'fovealDegree', 0.25, 'display', display.CRT12BitDisplay);
 retina = ConeResponse('eccBasedConeDensity', true, 'eccBasedConeQuantal', true, ...
-                      'fovealDegree', 0.125, 'display', display.CRT12BitDisplay);
+                      'fovealDegree', 1, 'display', display.CRT12BitDisplay);
+retina.visualizeMosaic();
 
 imageSize = [32, 32, 3];
 testImage = reshape(image_all(1, :), imageSize);
 [~, ~, testLinearImage, testConeVec] = retina.compute(testImage);
 
-nImage = 1e3;
+%% Generate dataset for learning render matrix
+nImage = 5e3;
 allConeVec = zeros(nImage, length(testConeVec));
 allLinearImage = zeros(nImage, length(testLinearImage(:)));
 
 parfor idx = 1:nImage
     
-    inputImage = reshape(image_all(idx, :), imageSize);   
+    inputImage = reshape(image_all(idx, :), imageSize);
     [~, ~, linearImage, coneExcitation, ~, ~, ~] = retina.compute(inputImage);
     
     allConeVec(idx, :) = coneExcitation;
@@ -48,7 +47,7 @@ load(fullfile(basisInDir, 'image_cifar_all_linear.mat'));
 nSample = 36;
 for idx = 1:nSample
     subplot(6, 6, idx);
-    testIdx =randi([1, size(image_all, 1)]);    
+    testIdx =randi([1, size(image_all, 1)]);
     inputImage = reshape(image_all(testIdx, :), imageSize);
     
     [~, ~, linearImage, gt, ~, ~, ~] = retina.compute(inputImage);
@@ -56,10 +55,10 @@ for idx = 1:nSample
     
     scatter(gt, recon); grid on; hold on;
     refPoint = [-500, 4000];
-    plot(refPoint, refPoint);    
+    plot(refPoint, refPoint);
     axis square;
     xlim(refPoint);
-    ylim(refPoint);       
+    ylim(refPoint);
 end
 suptitle('Render Matrix Approximation');
 
@@ -81,15 +80,13 @@ estimatorLasso = LassoGaussianEstimator(renderMatrix, regBasis, MU');
 [~] = visualizeBasis(estimatorRidge.Basis, 32, size(estimatorRidge.Basis, 2), false);
 [~] = visualizeBasis(estimatorLasso.Basis, 32, size(estimatorLasso.Basis, 2), false);
 
-retina.visualizeMosaic();
-
 %% Set some parameters for the estimation
 estimatorRidge.setLambda(1);
 
 estimatorLasso.dispOn();
 estimatorLasso.setLambda(1);
-estimatorLasso.setTolerance(1e-8);
-estimatorLasso.setIterationLimit(5e4);
+estimatorLasso.setTolerance(1e-6);
+estimatorLasso.setIterationLimit(1e4);
 
 %% Simple evaluation
 testIdx   = randi([9e4, size(image_all, 1)]);
@@ -116,3 +113,66 @@ title('Gaussian Prior');
 subplot(1, 3, 3);
 imshow(reconLasso, 'InitialMagnification', 500);
 title('Lasso Prior');
+
+%% Simple color test
+stimulus = 0.1 * ones(imageSize);
+stimulus(:, :, 1) = 0.9 * ones([32, 32]);
+[~, ~, ~, coneRes] = retina.compute(stimulus);
+
+reconRidge = estimatorRidge.estimate(coneRes');
+reconRidge = invGammaCorrection(reshape(reconRidge, imageSize), display.CRT12BitDisplay);
+
+reconLasso = estimatorLasso.estimate(coneRes');
+reconLasso = invGammaCorrection(reshape(reconLasso, imageSize), display.CRT12BitDisplay);
+
+figure;
+subplot(1, 3, 1);
+imshow(stimulus, 'InitialMagnification', 500);
+title('Original');
+
+subplot(1, 3, 2);
+imshow(reconRidge, 'InitialMagnification', 500);
+title('Gaussian Prior');
+
+subplot(1, 3, 3);
+imshow(reconLasso, 'InitialMagnification', 500);
+title('Lasso Prior');
+
+%% Single cone stimulation experiment
+estimatorRidge.setLambda(1);
+estimatorLasso.setLambda(1);
+
+nRow = 10;
+nCol = 10;
+
+h1 = figure(1);
+h2 = figure(2);
+for i = 1 : nRow
+    for j = 1 : nCol
+        stimulus = 0.5 * ones(imageSize);
+        [~, ~, ~, coneRes] = retina.compute(stimulus);
+        
+        stimIdx = randi([1, size(coneRes, 1)]);
+        coneRes(stimIdx) = coneRes(stimIdx) * 5;
+        
+        reconRidge = estimatorRidge.estimate(coneRes');
+        reconRidge = invGammaCorrection(reshape(reconRidge, imageSize), display.CRT12BitDisplay);
+        
+        reconLasso = estimatorLasso.estimate(coneRes');
+        reconLasso = invGammaCorrection(reshape(reconLasso, imageSize), display.CRT12BitDisplay);
+        
+        set(0, 'CurrentFigure', h1)
+        subplot(nRow, nCol, (i - 1) * nCol + j);
+        imshow(reconRidge, 'InitialMagnification', 500);        
+        
+        set(0, 'CurrentFigure', h2)
+        subplot(nRow, nCol, (i - 1) * nCol + j);
+        imshow(reconLasso, 'InitialMagnification', 500);        
+    end
+end
+
+set(0, 'CurrentFigure', h1)
+suptitle('Gaussian');
+
+set(0, 'CurrentFigure', h2)
+suptitle('Lasso');
