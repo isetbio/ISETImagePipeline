65f779d7f5b077d5a3b554775d45ca772d447b0d
lingqiz <zlqzcc@gmail.com>
2020-02-05 11:34:53 -0500

    add cross-validation

--- tutorials/t_reconDemo.m
+++ tutorials/t_reconDemo.m
@@ -12,7 +12,7 @@ thisImageSet = 'ILSVRC';
 imageName    = 'ILSVRC2017_test_00000021.JPEG';
 
 fileDir = fullfile(dataBaseDir, thisImageSet, imageName);
-image   = imresize(im2double(imread(fileDir)), 0.5);
+image   = imresize(im2double(imread(fileDir)), 0.4);
 
 patch = sampleImage(image, imageSize(1));
 patch(patch < 0) = 0;
@@ -61,4 +61,33 @@ title('reconstruction');
 retina.reconValidation(patch, reconImage, imageSize(1), response, estimator);
 
 %% Cross-validation
+meanResp = render * patchLinear(:);
+response = poissrnd(meanResp);
 
+regPara = [1e-8, 1e-6, 1e-4, 1e-3, 0.01, 0.05, 0.1, 0.5, 1];
+reconResult = zeros([length(regPara), imageSize]);
+
+parfor idx = 1:length(regPara)
+    
+    estimator = PoissonSparseEstimator(render, inv(regBasis), MU', regPara(idx), 4, imageSize);
+    reconResult(idx, :, :, :) = invGammaCorrection(estimator.estimate(response, 1.2e3, rand([prod(imageSize), 1]), true), display.CRT12BitDisplay);        
+    
+end
+
+%% Results of cross-validation
+reconLoss = zeros(1, length(regPara));
+figure();
+for idx = 1:length(regPara)
+    subplot(3, 3, idx);
+    reconRGB = reshape(reconResult(idx, :, :, :), imageSize);
+    imshow(reconRGB, 'InitialMagnification', 400);
+    reconLoss(idx) = sqrt(mean((patch(:) - reconRGB(:)) .^ 2));
+end
+
+figure();
+plot(log(regPara), reconLoss, '-ok', 'LineWidth', 1);
+set(gca,'box','off');
+set(gca,'TickDir','out');
+
+xticks(log(regPara));
+xticklabels(regPara);
