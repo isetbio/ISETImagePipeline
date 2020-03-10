94dc1ed9266ee8d625a08052f09018a36b46ea58
lingqiz <zlqzcc@gmail.com>
2020-03-10 15:54:33 -0400

    add main code

---
+++ peripheral/main.m
@@ -0,0 +1,22 @@
+% Saved variable
+load('sparsePrior.mat');
+load('inputImage_128.mat');
+
+load('retinaRenderFov.mat');
+render = double(renderFov);
+
+regPara = 1e-3;
+
+estimator = PoissonSparseEstimator(render, inv(regBasis), MU', regPara, 4, imageSize);
+outputImage = zeros(size(inputImage));
+
+parfor idx = 1:size(inputImage, 1)
+    
+    input = inputImage(idx, :, :, :);
+    coneVec = render * input(:);
+    
+    outputImage(idx, :, :, :) = estimator.estimate(coneVec, 1.5e3, rand([prod(imageSize), 1]), true);
+    
+end
+
+save('output.mat', 'outputImage', '-v7.3');
