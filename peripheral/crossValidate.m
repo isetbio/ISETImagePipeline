ee8ab0d1f3e3ad5335c3da8265b5facaa4ddd705
lingqiz <zlqzcc@gmail.com>
2020-03-12 11:20:03 -0400

    add plotting

--- peripheral/crossValidate.m
+++ peripheral/crossValidate.m
@@ -1,13 +1,14 @@
-% Saved variable
+%% Saved variable
 load('sparsePrior.mat');
 load('inputImage_128.mat');
 
 load('retinaRender10.mat');
 render = double(render10);
 
-regParas = [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1];
+regParas = [1e-5, 1e-4, 5e-4, 1e-3, 1e-2, 5e-2, 0.1, 1];
 outputImage = zeros([length(regParas), size(inputLinear)]);
 
+%% Run reconstruction
 for regIdx = 1:length(regParas)
     regPara = regParas(regIdx);
     estimator = PoissonSparseEstimator(render, inv(regBasis), MU', regPara, 4, imageSize);
@@ -22,3 +23,21 @@ for regIdx = 1:length(regParas)
 end
 
 save('cvOutput.mat', 'outputImage', '-v7.3');
+
+%% Show results
+dataBaseDir = getpref('ISETImagePipeline', 'dataDir');
+display = load(fullfile(dataBaseDir, 'CRT12BitDisplay.mat'));
+
+figure();
+plotAxis = tight_subplot(length(regParas), 12, [0.01, 0.01], 0.01, 0.01);
+
+plotID = 1;
+for i = 1:length(regParas)
+    for j = 1:size(inputLinear, 1)
+        axes(plotAxis(plotID));
+        image = reshape(outputImage(i, j, :, :, :), imageSize);
+        imshow(invGammaCorrection(image, display.CRT12BitDisplay), 'InitialMagnification', 400);
+        
+        plotID = plotID + 1;
+    end
+end
