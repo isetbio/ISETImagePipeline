9631d39832b6d72330dfb619250bff1db6121c8c
lingqiz <zlqzcc@gmail.com>
2021-04-06 11:24:34 -0400

    set up full field calculation

--- peripheral/fullField.m
+++ peripheral/fullField.m
@@ -1,5 +1,9 @@
 %% Compute reconstruction from a LARGE field
-nStep = 18;
+eccX = -2.5 : 1 : 14.5;
+eccY = 14.5 : -1 : -2.5;
+
+nStep = length(eccX);
+assert(nStep == length(eccY));
 
 imgEdge = 64;
 imageSize = [64, 64, 3];
@@ -11,15 +15,12 @@ input = im2double(imread('./imgLarge.jpeg'));
 input = sampleImage(input, nStep * imageSize(1));
 output = zeros(size(input));
 
-eccX = -2.5 : 1 : 14.5;
-eccY = 14.5 : -1 : -2.5;
-
 for idx = 1 : nStep
     for idy = 1 : nStep
         retina = ConeResponseCmosaic...
             (eccX(idx), eccY(idy), 'fovealDegree', 1.0, 'pupilSize', 3.0, 'subjectID', 9);
         
-        render = retina.forwardRender(imageSize, false, false);
+        render = retina.forwardRender(imageSize, false, false);        
         render = double(render);
         
         regPara = 2e-3; stride = 4;
@@ -29,10 +30,12 @@ for idx = 1 : nStep
         inputPatch = input(cvtIdx(idx, imgEdge), cvtIdx(idy, imgEdge), :);
         
         response = render * inputPatch(:);
-        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'iter');
+        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'off');        
         
         output(cvtIdx(idx, imgEdge), cvtIdx(idy, imgEdge), :) = ...
             gammaCorrection(recon, display);
+        
+        fprintf('x: %d, y: %d \n', idx, idy);
     end
 end
 
