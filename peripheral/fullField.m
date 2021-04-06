1d519861f9f21ddaff984eecf29c8ec2b720a161
lingqiz <zlqzcc@gmail.com>
2021-04-06 11:17:21 -0400

    full field recon

---
+++ peripheral/fullField.m
@@ -0,0 +1,47 @@
+%% Compute reconstruction from a LARGE field
+nStep = 18;
+
+imgEdge = 64;
+imageSize = [64, 64, 3];
+
+display = displayCreate('CRT12BitDisplay');
+prior = load('../sparsePrior.mat');
+
+input = im2double(imread('./imgLarge.jpeg'));
+input = sampleImage(input, nStep * imageSize(1));
+output = zeros(size(input));
+
+eccX = -2.5 : 1 : 14.5;
+eccY = 14.5 : -1 : -2.5;
+
+for idx = 1 : nStep
+    for idy = 1 : nStep
+        retina = ConeResponseCmosaic...
+            (eccX(idx), eccY(idy), 'fovealDegree', 1.0, 'pupilSize', 3.0, 'subjectID', 9);
+        
+        render = retina.forwardRender(imageSize, false, false);
+        render = double(render);
+        
+        regPara = 2e-3; stride = 4;
+        estimator = PoissonSparseEstimator...
+            (render, inv(prior.regBasis), prior.mu', regPara, stride, imageSize);
+        
+        inputPatch = input(cvtIdx(idx, imgEdge), cvtIdx(idy, imgEdge), :);
+        
+        response = render * inputPatch(:);
+        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'iter');
+        
+        output(cvtIdx(idx, imgEdge), cvtIdx(idy, imgEdge), :) = ...
+            gammaCorrection(recon, display);
+    end
+end
+
+%% Helper function
+function index = cvtIdx(index, step)
+
+startIdx = (index - 1) * step + 1;
+endIdx = startIdx + step - 1;
+
+index = startIdx : 1 : endIdx;
+
+end
