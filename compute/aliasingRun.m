7be0ef3c2325957bb7a8a4dd5e5508941eb1f6ac
lingqiz <zlqzcc@gmail.com>
2021-04-08 16:55:02 -0400

    test special scene

--- compute/aliasingRun.m
+++ compute/aliasingRun.m
@@ -31,7 +31,7 @@ catch NoGPU
     useGPU = false;
 end
 
-%% Run image reconstruction
+%% Test run - image reconstruction
 figure();
 inputSize = [1024, 1024, 3];
 outputOptics = zeros([6, imageSize]);
@@ -54,6 +54,43 @@ end
 % Use a large pupil size to reduce the effect of diffraction
 diffPupil = 10.0;
 retina.PSF = ConeResponse.psfDiffLmt(diffPupil);
+
+%% Run reconstruction with special scene
+% stimFreq = [2, 6, 12, 16, 20, 25, 32, 64, 96, 128];
+stimFreq = [8, 16, 32, 64, 96, 128];
+rmsContrast = 1.0;
+chromaDir = [1.0, 1.0, 1.0]';
+chromaDir = chromaDir / norm(chromaDir) * rmsContrast;
+
+outputDiflmt = zeros([length(stimFreq), imageSize]);
+
+figure();
+for idx = 1:length(stimFreq)
+    gratingScene = ...
+        createGratingScene(chromaDir, stimFreq(idx), 'fovDegs', fovDegs, 'pixelsNum', 2048);
+    
+    crst = 0.50;
+    [theSceneSequence, ~] = gratingScene.compute(crst);
+    scene = theSceneSequence{:};
+    
+    % Special scene with narrow bandwith
+    scene.data.photons(:, :, (scene.spectrum.wave ~= 620 & scene.spectrum.wave ~= 640)) = 0.0;
+    
+    % Compute response from scene
+    allCone = retina.computeWithScene(scene);
+    % visualizeOpticalImage(retina.LastOI, 'displayRadianceMaps', true, ...
+    %    'displayRetinalContrastProfiles', true);
+    
+    reconImage = ...
+        estimator.runEstimate(allCone, 'maxIter', 150, ...
+        'display', 'iter', 'gpu', useGPU);
+    
+    outputDiflmt(idx, :, :, :) = reconImage;
+    
+    subplot(2, ceil(length(stimFreq)/2), idx);
+    imshow(gammaCorrection(reconImage, display), 'InitialMagnification', 500);
+    
+end
 
 %% Run image reconstruction
 figure();
@@ -102,8 +139,11 @@ gratingScene = ...
 crst = 0.50;
 [theSceneSequence, ~] = gratingScene.compute(crst);
 
+scene = theSceneSequence{:};
+scene.data.photons(:, :, (scene.spectrum.wave ~= 620 & scene.spectrum.wave ~= 640)) = 0.0;
+
 % Compute response from scene
-[~, allCone] = retina.computeWithScene(theSceneSequence{:});
+[~, allCone] = retina.computeWithScene(scene);
 visualizeOpticalImage(retina.LastOI, 'displayRadianceMaps', true, ...
     'displayRetinalContrastProfiles', true);
 
@@ -182,9 +222,10 @@ for idx = 1:length(stimFreq)
     
     crst = 0.20;
     [theSceneSequence, ~] = gratingScene.compute(crst);
-    
+    scene = theSceneSequence{:};
+            
     % Compute response from scene
-    allCone = retina.computeWithScene(theSceneSequence{:});
+    allCone = retina.computeWithScene(scene);
     % visualizeOpticalImage(retina.LastOI, 'displayRadianceMaps', true, ...
     %    'displayRetinalContrastProfiles', true);
     
