4208d49249cb4dfc2c5617c6ce044bf9ee5b99a3
lingqiz <zlqzcc@gmail.com>
2021-04-10 13:13:59 -0400

    save renders

--- peripheral/fullField.m
+++ peripheral/fullField.m
@@ -6,17 +6,13 @@ cd ./peripheral;
 cond = 2;
 switch cond
     case 1
-        
         eccX = 0.5 : 1 : 14.5;
         eccY = 14.5 : -1 : 0.5;
         fovDegs = 1.0;
-        
     case 2
-        
         eccX = 5 : 2 : 15;
         eccY = 15 : -2 : 5;
         fovDegs = 2.0;
-        
     otherwise
         error('Invalid Condition');
 end
@@ -30,8 +26,8 @@ numY = length(eccY);
 display = displayCreate('CRT12BitDisplay');
 prior = load('./sparsePrior.mat');
 
-input = load('./largeLinear.mat');
-input = input.inputLinear;
+input = load('./largeLinear0.mat');
+input = input.input;
 input = imresize(input, imgEdge * numX / size(input, 1));
 
 %% Run computation
@@ -43,17 +39,25 @@ for idx = 1 : numX
     startX = (idx - 1) * imgEdge + 1;
     endX = startX + imgEdge - 1;
     
-    for idy = 1 : numY
-        retina = ConeResponseCmosaic...
-            (eccX(idx), eccY(idy), 'fovealDegree', fovDegs, 'pupilSize', 3.0, 'subjectID', 9);
-        
-        render = retina.forwardRender(imageSize, false, false);
-        renderArray{idy} = double(render);
-    end
+    fileName = strcat('./renderArray/', sprintf('rnd_x%d', idx), '.mat');
     
+    if isfile(fileName)
+        savedRender = load(fileName);
+        renderArray = savedRender.renderArray;
+    else
+        for idy = 1 : numY
+            retina = ConeResponseCmosaic...
+                (eccX(idx), eccY(idy), 'fovealDegree', fovDegs, 'pupilSize', 3.0, 'subjectID', 9);
+            
+            render = retina.forwardRender(imageSize, false, false);            
+            renderArray{idy} = double(render);
+        end
+        save(fileName, 'renderArray');
+    end
+        
     fprintf('Finished render matrix calculation \n');
     
-    parfor idy = 1 : numY
+    for idy = 1 : numY
         regPara = 1.5e-3; stride = 4;
         estimator = PoissonSparseEstimator...
             (renderArray{idy}, inv(prior.regBasis), prior.mu', regPara, stride, imageSize);
@@ -64,7 +68,7 @@ for idx = 1 : numX
         inputPatch = input(startY:endY, startX:endX, :);
         
         response = render * inputPatch(:);
-        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'off');
+        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'off');        
         
         outputArray{idy} = gammaCorrection(recon, display);
     end
@@ -74,8 +78,7 @@ for idx = 1 : numX
         endY = startY + imgEdge - 1;
         
         output(startY:endY, startX:endX, :) = outputArray{idy};
-    end
-    
+    end    
     fprintf('Current x: %d / %d \n', idx, numX);
 end
 
