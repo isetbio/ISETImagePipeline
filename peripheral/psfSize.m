34e70404f753c613de0ac40a16382003f2d584ca
lingqiz <zlqzcc@gmail.com>
2021-05-08 11:58:32 -0400

    new analysis

---
+++ peripheral/psfSize.m
@@ -0,0 +1,47 @@
+%% Load the display setup and create mosaic object
+load('inputLinear.mat');
+
+display = displayCreate('CRT12BitDisplay');
+prior = load('../sparsePrior.mat');
+imageSize = [100, 100, 3];
+
+retinaEccs = [1, 0; 5, 0; 10, 0; 10, 10; 18, 0; 18, 18];
+nRetina = size(retinaEccs, 1);
+
+output = zeros([nRetina, size(inputLinear, 1), imageSize]);
+
+ecc = 10;
+retina = ConeResponseCmosaic...
+    (ecc, ecc, 'fovealDegree', 1.0, 'pupilSize', 3.0, 'subjectID', 9);
+
+%% Run reconstructions
+for retinaID = 1 : nRetina
+    fprintf('Run ID: %d \n', retinaID);
+    
+    % Generate cone mosaic - [eccX, eccY] deg ecc
+    eccX = retinaEccs(retinaID, 1);
+    eccY = retinaEccs(retinaID, 2);
+    
+    retina_ = ConeResponseCmosaic...
+        (eccX, eccY, 'fovealDegree', 1.0, 'pupilSize', 3.0, 'subjectID', 9);
+    
+    retina.PSF = retina_.PSF;
+    retina.psfData = retina_.psfData;
+    
+    render = retina.forwardRender(imageSize, false, false);
+    render = double(render);
+    
+    regPara = 2e-3; stride = 4;
+    estimator = PoissonSparseEstimator...
+        (render, inv(prior.regBasis), prior.mu', regPara, stride, imageSize);
+    
+    parfor idx = 1 : size(inputLinear, 1)
+        input = reshape(inputLinear(idx, :, :, :), [128, 128, 3]);
+        input = imresize(input, imageSize(1) / size(input, 1));
+        
+        response = render * input(:);
+        recon = estimator.runEstimate(response, 'maxIter', 500, 'display', 'iter');
+        
+        output(retinaID, idx, :, :, :) = recon;
+    end
+end
