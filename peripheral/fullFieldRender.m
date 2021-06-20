a0b078b0f8cc7ae30ac9936680be1a7cbe338b36
lingqiz <zlqzcc@gmail.com>
2021-06-20 16:26:15 -0400

    generate large filed render matrix

---
+++ peripheral/fullFieldRender.m
@@ -0,0 +1,32 @@
+% Generate render matrix across eccentricity
+
+% Parameters
+eccX = -2.5 : 1 : 14.5;
+eccY = 14.5 : -1 : -2.5;
+fovDegs = 1.0;
+
+imgEdge = 128;
+imageSize = [imgEdge, imgEdge, 3];
+
+display = displayCreate('CRT12BitDisplay');
+
+numX = length(eccX);
+numY = length(eccY);
+renderArray = cell(numX, numY);
+
+% Loop through cone mosaics
+for idx = 1:numX
+    for idy = 1:numY
+        retina = ConeResponseCmosaic...
+            (eccX(idx), eccY(idy), 'fovealDegree', fovDegs, 'pupilSize', 3.0, 'subjectID', 6);
+        
+        render = retina.forwardRender(imageSize, false, false);
+        [~, ~, v] = svd(render, 'econ');
+        
+        renderArray{idx, idy} = single(v);
+    end
+end
+
+% Save results
+save('renderArray.mat', 'renderArray', ...
+    'eccX', 'eccY', 'imageSize', '-v7.3');
