63b2b94e926eb9734e2be735d5ffbfca428e5c92
lingqiz <zlqzcc@gmail.com>
2021-11-14 13:09:48 -0500

    minor update

--- peripheral/periMLE.m
+++ peripheral/periMLE.m
@@ -1,46 +1,45 @@
 %% setup
-imageSize = [128, 128, 3];
+imageSize = [100, 100, 3];
 display = displayCreate('CRT12BitDisplay');
-prior   = load('sparsePrior.mat');
+prior = load('sparsePrior.mat');
 
-%% load matrix
-load('retinaRender40.mat');
-render = double(render40);
+renderMtx = load('periRender.mat');
+renderMtx = renderMtx.renderMtx;
 
 %% show image
-load('./inputLinear.mat');
-inputLinear = inputLinear([1, 3, 6, 7, 9, 10, 11, 12], :, :, :);
+load('inputLinear.mat');
 
 figure();
 for idx = 1 : size(inputLinear, 1)
-    subplot(2, 4, idx);
+    subplot(2, 5, idx);
     image = reshape(inputLinear(idx, :, :, :), imageSize);
     imshow(gammaCorrection(image, display), 'InitialMagnification', 200);
 end
 
 %% run reconstruction
-% Build an image reconstruction object with sparse prior
-regConst = 0.0; stride = 4;
-estimator = ...
-    PoissonSparseEstimator(render, inv(prior.regBasis), prior.mu', regConst, stride, imageSize);
-
-% Run reconstruction on cone response to each images
-% reconstructed images are in linear pixel space, need to
-% gamma correct them before visulization
-nIter = 800; optDisp = 'iter';
-output = zeros(size(inputLinear));
-
-parfor idx = 1 : size(inputLinear, 1)
-    input = reshape(inputLinear(idx, :, :, :), imageSize);
-    coneResp = render * input(:);
-    recon = estimator.runEstimate(coneResp, 'maxIter', nIter, 'display', optDisp);
-    output(idx, :, :, :) = gammaCorrection(recon, display);
+allOutput = cell(5, 1);
+for rid = 1:5
+    render = double(renderMtx{rid});
+    
+    % Build an image reconstruction object with sparse prior
+    regConst = 0.0; stride = 4;
+    estimator = PoissonSparseEstimator(render, inv(prior.regBasis), ...
+                                prior.mu', regConst, stride, imageSize);
+    
+    % Run reconstruction on cone response to each images
+    % reconstructed images are in linear pixel space, need to
+    % gamma correct them before visulization
+    nIter = 800; optDisp = 'final';
+    output = zeros(size(inputLinear));
+    
+    parfor idx = 1 : size(inputLinear, 1)
+        input = reshape(inputLinear(idx, :, :, :), imageSize);
+        coneResp = render * input(:);
+        recon = estimator.runEstimate(coneResp, 'maxIter', nIter, 'display', optDisp);
+        output(idx, :, :, :) = gammaCorrection(recon, display);
+    end
+    
+    allOutput{rid} = output;
 end
 
-%% show results
-figure();
-for idx = 1 : size(output, 1)
-    subplot(2, 4, idx);
-    reconImage = reshape(output(idx, :, :, :), imageSize);
-    imshow(reconImage, 'InitialMagnification', 200);
-end
+save('reconOut.mat', 'imageSize', 'allOutput', '-v7.3');
