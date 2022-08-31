1f101a88a16de9e0ed2efd11a93d410a2393ba65
David Brainard <brainard@psych.upenn.edu>
2022-08-31 13:30:31 -0400

    Gamma correction and incorporate lens into fundamentals

--- recon_ao/stimManipulationExample.m
+++ recon_ao/stimManipulationExample.m
@@ -24,27 +24,57 @@ aoReconDir = getpref('ISETImagePipeline','aoReconDir'); helpDir = '/helperFiles'
 theDisplayLoad = load(fullfile(aoReconDir,helpDir,[displayName 'Display.mat']));
 eval(['theDisplay = theDisplayLoad.' displayFieldName ';']);
 
-% Account for the gamma functions 
-
-
 % Show the stimulus by creating an ISETBio scene
 meanLuminanceCdPerM2 = [];
 [stimulusScene, ~, stimulusImageLinear] = sceneFromFile(stimulusImageRGB, 'rgb', ...
     meanLuminanceCdPerM2, theDisplay);
-stimulusImageRGB = gammaCorrection(stimulusImageLinear, theDisplay);
 stimulusScene = sceneSet(stimulusScene, 'fov', fieldSizeDegs);
 visualizeScene(stimulusScene, 'displayRadianceMaps', false, 'avoidAutomaticRGBscaling', true);
+
+% Explicitly do gamma correction and check below.  We'll need to be able to
+% do this lower down.
+stimulusImageRGB1 = gammaCorrection(stimulusImageLinear, theDisplay);
+
+% And becuase it's here, do inverse gamma correction the ISETBio way.
+% Note that currently there is an invGammaCorrection function on the
+% path that actually does the forward gammaCorrection.
+gammaLength = size(theDisplay.gamma,1);
+stimulusImageLinear1 = ieLUTDigital(round((gammaLength-1)*stimulusImageRGB1), theDisplay.gamma);
+
+% Check that things we think should match actually do.  Don't expect exact
+% agreement because gamma table is discrete.
+examplePixelStimulusRGB = squeeze(stimulusImageRGB(50,50,:));
+if (any(stimRGB ~= examplePixelStimulusRGB))
+    error('Stimulus not built the way we expect');
+end
+examplePixelStimulusRGB1 = squeeze(stimulusImageRGB1(50,50,:));
+if (max(abs(examplePixelStimulusRGB1-stimRGB))/mean(stimRGB) > 1e-2)
+    error('Explict gamma correction from linear values doesn''t match RGB input');
+end
+examplePixelLinear = squeeze(stimulusImageLinear(50,50,:));
+examplePixelLinear1 = squeeze(stimulusImageLinear1(50,50,:));
+if (max(abs(examplePixelLinear-examplePixelLinear1))/mean(examplePixelLinear) > 1e-3)
+    error('Explict gamma correction from linear values doesn''t match RGB input');
+end
 
 % Grab the primaries of the display
 B_primary = theDisplay.spd;
 wls = theDisplay.wave;
 
-% Build a mosaic
+% Build a mosaic object and pull out parts we need.
 theConeMosaic = ConeResponseCmosaic(0, 0, ...
         'fovealDegree', fieldSizeDegs, 'pupilSize', 3, 'useRandomSeed', true);
+
+% Get lens transmittance.  Should really to this using a get
+theOI = theConeMosaic.PSF;
+theOptics = oiGet(theOI,'optics');
+theLens = opticsGet(theOptics,'lens');
+lensTransmittance = theLens.transmittance';
+
 theMosaic = theConeMosaic.Mosaic;
 coneWls = theMosaic.wave;
-coneQE = theMosaic.qe';
+coneQENoLens = theMosaic.qe';
+coneQE = coneQENoLens .* lensTransmittance(ones(size(coneQENoLens,1),1),:);
 coneQESpline = SplineCmf(coneWls,coneQE,wls);
 coneFundamentals = EnergyToQuanta(wls,coneQESpline')';
 figure; clf; hold on; plot(wls,coneFundamentals','r');
@@ -54,4 +84,5 @@ coneExcitations = (coneFundamentals*B_primary)*stimRGB;
 
 % Perturb M cones
 coneExcitations1 = coneExcitations + [0 0.05*coneExcitations(2) 0]';
-stimRGB1 = inv(coneFundamentals*B_primary)*coneExcitations1;
+stimPerturbMLinear = inv(coneFundamentals*B_primary)*coneExcitations1;
+stimPerturbMRGB = gammaCorrection(stimPerturbMLinear, theDisplay);
