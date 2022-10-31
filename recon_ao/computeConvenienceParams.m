9439639d65134ab0dfa9cdd5a9c9ab76d69f6c5c
David Brainard <brainard@psych.upenn.edu>
2022-10-31 12:53:58 -0400

    Type fix

--- recon_ao/computeConvenienceParams.m
+++ recon_ao/computeConvenienceParams.m
@@ -106,8 +106,8 @@ if (length(pr.stimBgVal) > 1)
 else
     cnv.outputSubName = sprintf('%0.1f_%0.4f_%d_%0.4f_%0.4f_%0.4f_%0.4f_%s_%s_%d_%s_%d_%d_%d_%s_%d', ...
         60*pr.stimSizeDegs, pr.regPara,pr.stride,pr.stimBgVal,pr.stimRVal,pr.stimGVal,pr.stimBVal, cnv.exciteSource, pr.forwardChrom, pr.forwardEccVars, pr.reconChrom, pr.reconEccVars, ...
-        pr.stimCenter(1),pr.stimCenter(2),noiseStr);
+        pr.stimCenter(1),pr.stimCenter(2),noiseStr,pr.boundedSearch);
 end
-cnv.outputDir = fullfile(pr.aoReconDir ,cnv.outputMainName,cnv.outputSubName,pr.boundedSearch);
+cnv.outputDir = fullfile(pr.aoReconDir ,cnv.outputMainName,cnv.outputSubName);
 
 end
