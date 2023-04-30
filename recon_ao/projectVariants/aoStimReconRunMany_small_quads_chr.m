21e8940543716bf2d78dba799434ebbd861ae133
CarlosRod178 <chr4qt@virginia.edu>
2023-04-30 00:07:03 -0400

    More Quad Seqs and Plots

--- recon_ao/projectVariants/aoStimReconRunMany_small_quads_chr.m
+++ recon_ao/projectVariants/aoStimReconRunMany_small_quads_chr.m
@@ -26,6 +26,7 @@ clear; close all;
 rrf = struct;
 rrf.rerunImages = false;
 rrf.slidePlots = false;
+rrf.statPlots = false;
 rrf.trueDisplayName = 'mono';
 rrf.viewingDisplayName = 'conventional';
 rrf.stimDispScale = 3;
@@ -34,9 +35,19 @@ rrf.reconDispScale = 1;
 if (rrf.slidePlots)
     rows = 12;
     colms = 13;
+    mosaicSpread =  fliplr([0:2:20] / 20); %fliplr([0:8] / 8);
+    sizes = [10.0 2.0 3.5 5.0 6.5]; % [10.5 2.5 3.5 4.5 6.5]; 
+    fig1Legend = {'2.5 arcmin', '3.5 arcmin', '4.5 arcmin', '6.5 arcmin', '10.5 arcmin'};
+    fig2Legend = {'1.0 L:M', '0.9 L:M', '0.8 L:M', '0.7 L:M', '0.6 L:M', '0.5 L:M', '0.4 L:M', '0.3 L:M', '0.2 L:M', '0.1 L:M', '0 L:M'};
+    % {'1.00 L:M', '0.88 L:M', '0.75 L:M', '0.63 L:M', '0.50 L:M','0.38 L:M', '0.25 L:M', '0.13 L:M', '0.00 L:M'};
+
     cellRecons = cell(rows-1, colms);
+    cellStatsRecon = cell(size(cellRecons));
+
     stimGrab = [1:colms] *  (rows-1);
     cellStim = cell(1,colms);
+    cellStatsStim = cell(size(cellStim));
+    
 end
 
 if (rrf.rerunImages)
@@ -50,24 +61,55 @@ if (rrf.rerunImages)
         counter = 1;
         rrf.mainDir = fullfile(rrf.wrapDir, rrf.wrapDirInfo(i).name);
         rrf.mainDirInfo = dir(rrf.mainDir);
-       
+
         for j = 4:length(rrf.mainDirInfo)
-            rrf.outputDir = fullfile(rrf.mainDir, rrf.mainDirInfo(j).name);
-           
-            if(rrf.slidePlots)
-                load(fullfile(rrf.outputDir, 'xRunOutput.mat'),"reconRGBDispCorrectedBoost", "stimRGBDispCorrectedBoost")
-                if ismember(counter, stimGrab)
-                    cellStim{1, counter/(rows-1)} = stimRGBDispCorrectedBoost;
+            if rrf.mainDirInfo(j).isdir
+                rrf.outputDir = fullfile(rrf.mainDir, rrf.mainDirInfo(j).name);
+
+                if(rrf.slidePlots)
+                    load(fullfile(rrf.outputDir, 'xRunOutput.mat'), ...
+                        "reconRGBDispCorrectedBoost", "stimRGBDispCorrectedBoost", ...
+                        "rgbStatsStim", "rgbStatsRecon")
+
+                    cellRecons{counter} = reconRGBDispCorrectedBoost;
+                    cellStatsRecon{counter} = rgbStatsRecon;
+
+                    if ismember(counter, stimGrab)
+                        cellStim{1, counter/(rows-1)} = stimRGBDispCorrectedBoost;
+                        cellStatsStim{1, counter/(rows-1)} = rgbStatsStim;
+                    end
+
+                    counter = counter + 1;
+
+                else
+                    load(fullfile(rrf.outputDir, 'xRunOutput.mat'), "pr", "cnv")
+                    rrf.renderDir = fullfile(rrf.aoReconDir, rrf.versEditor);
+                    aoStimRecon(pr, cnv, rrf)
+
                 end
-                cellRecons{counter} = reconRGBDispCorrectedBoost;
-                counter = counter + 1;
-            else
-                load(fullfile(rrf.outputDir, 'xRunOutput.mat'), "pr", "cnv")
-                rrf.renderDir = fullfile(rrf.aoReconDir, rrf.versEditor);
-                aoStimRecon(pr, cnv, rrf)
             end
         end
 
+        rrgValsStim = ones(1,colms);
+        for k = 1:colms
+            cellStats{1,k} = cellStatsStim(k);
+            cellStats{2,k} = cellStatsRecon(:,k);
+            rrgValsRecons = [];
+
+            for q = 1:(rows-1)
+                rrgValsRecons = [rrgValsRecons cellStatsRecon{q,k}{1,5}];
+            end
+            rrgValsStim(k) = cellStatsStim{1,k}{1,5};
+            cellStats{3,k} = (rrgValsRecons);
+            cellStats{4,k} = mean(rrgValsRecons);
+            cellStats{5,k} = std(rrgValsRecons);
+            cellStats{6,k} = mosaicSpread;
+        end
+
+
+        save(fullfile(rrf.mainDir, "cellStats.mat"), "cellStats")
+
+        
         if (rrf.slidePlots)
             cellFull = [cellStim; cellRecons];
             figFull = imtile(cellFull, 'GridSize', [colms, rows]);
@@ -76,7 +118,95 @@ if (rrf.rerunImages)
         end
     end
 
+    if (rrf.statPlots)
+        cellStatsAll = cell(1,(length(rrf.wrapDirInfo)-3));
+        for i = 4:length(rrf.wrapDirInfo)
+            rrf.mainDir = fullfile(rrf.wrapDir, rrf.wrapDirInfo(i).name);
+            load(fullfile(rrf.mainDir, 'cellStats.mat'));
+            cellStatsAll{i-3} = cellStats;
+        end
+        
+        cellStatsAll = [num2cell(sizes);cellStatsAll];
 
+
+        % Temporary reorganizing since the above cycles through the directory
+        % names starting with "10" before "2"
+        holder=cellStatsAll(:,1);
+        cellStatsAll=cellStatsAll(:,2:end);
+        cellStatsAll(:,end+1)=holder;
+
+
+        % Figure 1
+        figure()
+        subplot(3,1,1)
+        for j=1:length(cellStatsAll)
+            plot(cellStatsAll{2,j}{6,7}, cellStatsAll{2,j}{3,7}, '-o'); hold on;
+        end
+        set(gca, 'XDir', 'reverse')
+        xlabel('L:M Ratio', 'FontSize', 14);
+        ylabel('$\frac{r}{r+g}$', 'Interpreter', 'latex', 'FontSize', 22);
+        title('Recon Primary Proportions over Cone Proportions', 'FontSize', 16);
+        legend(fig1Legend);
+
+        % Figure 2
+        subplot(3,1,2)
+        for i = 1:(rows-1)
+            tempStore = ones(1,length(cellStatsAll));
+            for j=1:length(cellStatsAll)
+                tempStore(j) = cellStatsAll{2,j}{3,7}(i);
+            end
+            plot(cell2mat(cellStatsAll(1,:)), tempStore, '-o'); hold on;
+        end
+        xlabel('Stimulus Size (arcmin)', 'FontSize', 14);
+        ylabel('$\frac{r}{r+g}$', 'Interpreter', 'latex', 'FontSize', 22);
+        title('Recon Primary Proportions over Stim Size',  'FontSize', 16);
+        legend(fig2Legend, 'NumColumns', 2);
+
+
+
+        % Figure 3
+        subplot(3,1,3)
+        %         for i = 1:(rows-1)
+        tempStore = ones(1,length(cellStatsAll));
+        for j=1:length(cellStatsAll)
+            tempStore(j) = cellStatsAll{2,j}{5,7};
+        end
+        plot(cell2mat(cellStatsAll(1,:)), tempStore, '-o'); hold on;
+        xlabel('Stimulus Size (arcmin)', 'FontSize', 14);
+        ylabel('Yellow Recon Variation', 'FontSize', 14);
+        title('Recon Variation over Stim Size', 'FontSize', 16);
+
+        
+        set(gcf, 'Position', [335     1   564   976]);
+        saveas(gcf,fullfile(rrf.wrapDir,'sumStatPlotsYellow.tiff'),'tiff')
+
+        for k = 1:length(cellStatsAll)
+            figure()
+            tempStore = ones(1,colms);
+            for i = 1:(rows-1)
+                for j = 1:colms
+                    tempStore(j) = cellStatsAll{2,k}{3,j}(i);
+                end
+                plot(fliplr([1:colms]), tempStore, '-o'); hold on
+            end
+            ylabel('$\frac{r}{r+g}$', 'Interpreter', 'latex', 'FontSize', 22);
+            xlabel('Stimulus', 'FontSize', 14);
+            legend(fig2Legend)
+            title(['Recon Progression over Stimuli: ', num2str(cellStatsAll{1,k}), ' arcmin'], 'FontSize', 16)
+            set(gcf, 'Position', [119   321   661   518])
+            saveas(gcf,fullfile(rrf.wrapDir,['reconProgress', num2str(cellStatsAll{1,k}), 'arcmin.tiff']),'tiff')
+        end
+
+
+%         Figure for the stimuli color progression
+%         figure()
+%         plot(fliplr([1:13]), rrgValsStim, '-o')
+%         ylabel('$\frac{r}{r+g}$', 'Interpreter', 'latex', 'FontSize', 22);
+%         xlabel('Stimulus', 'FontSize', 14);
+
+
+
+    end
 
 
 elseif ~(rrf.rerunImages)
@@ -119,8 +249,8 @@ elseif ~(rrf.rerunImages)
     %    "chromNorm", "chromProt", "chromDeut", "chromTrit",
     %    "chromAllL", "chromAllM", "chromAllS", "quadSeq" and number
     %    Currently established quadSeq1 - quadSeq56
-    forwardChromList = ["quadSeq68" "quadSeq69" "quadSeq70" "quadSeq71" "quadSeq72" "quadSeq73" "quadSeq74" "quadSeq75" "quadSeq76"]; % Don't forget to run QS34 on 4@0.5
-    reconChromList =   ["quadSeq68" "quadSeq69" "quadSeq70" "quadSeq71" "quadSeq72" "quadSeq73" "quadSeq74" "quadSeq75" "quadSeq76"]; % 36, 38, 40, 42, 44
+    forwardChromList = ["quadSeq77" "quadSeq78" "quadSeq79" "quadSeq80" "quadSeq81" "quadSeq82" "quadSeq83" "quadSeq84" "quadSeq85" "quadSeq86" "quadSeq87" ]; % Don't forget to run QS34 on 4@0.5
+    reconChromList =   ["quadSeq77" "quadSeq78" "quadSeq79" "quadSeq80" "quadSeq81" "quadSeq82" "quadSeq83" "quadSeq84" "quadSeq85" "quadSeq86" "quadSeq87"]; % 36, 38, 40, 42, 44
 
     % Build new sequence by
     prBase.quads(1).name = 'useQuadSeq';
@@ -129,7 +259,7 @@ elseif ~(rrf.rerunImages)
     % If want to apply percentages to the full mosaic instead of a
     % quadrant, set to true and use percentages corresponding to Quadrant 4
     % (prBase.quads(5))
-    fullMosaicPercent = false;
+    fullMosaicPercent = true;
 
     if(prBase.quads(1).value)
         % Initialize storage structure with information on each quadrant
@@ -144,7 +274,7 @@ elseif ~(rrf.rerunImages)
         prBase.quads(2).percentL = [0.27];
         prBase.quads(3).percentL = [0.53];
         prBase.quads(4).percentL = [0.71];
-        prBase.quads(5).percentL = [0.00];
+        prBase.quads(5).percentL = [0.70];
 
         % Enter desired percent as decimal of S cones per region across
         % quadrants. Follows same form as above
@@ -188,7 +318,7 @@ elseif ~(rrf.rerunImages)
     %% Stimulus parameters.
     %
     % Size list parameter in degs, expressed as min/60 (because 60 min/deg)
-    stimSizeDegsList = [2.5 3.5 4.5 6.5 10.5] / 60;
+    stimSizeDegsList = [2.0 3.5 5.0 6.5 10.5] / 60;
 
     % RGB values (before gamma correction)
     prBase.stimBgVal = 1;% [0.1054 0.1832 0.1189]
@@ -201,7 +331,7 @@ elseif ~(rrf.rerunImages)
     % staircase procedure where (-) is more red and (+) is more green,
     % (Intervals of 50s? 100s? 1000s?)
     isoLumRG = true;
-    colorStepRG = [-1280 -880 -480 0 240 480 720 960 1200 1440 1680];
+    colorStepRG = [-1729 -1280 -880 -480 0 240 480 720 960 1200 1440 1680 1729];
 
     if (isoLumRG)
         % Load the appropriate display
@@ -413,7 +543,7 @@ elseif ~(rrf.rerunImages)
     end
 
     % THIS SHOULD BE A PARFOR AFTERWARDS DON'T FORGET
-    for pp = 1:length(regPara)
+    parfor pp = 1:length(regPara)
 
         % Set up paramters structure for this loop, filling in fields that come
         % out of lists above.
