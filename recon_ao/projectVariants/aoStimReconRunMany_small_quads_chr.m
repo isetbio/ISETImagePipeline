5d00bca7aa36f683941fd5a7f3d37fe3dbdef48c
CarlosRod178 <chr4qt@virginia.edu>
2024-05-27 11:30:25 -0400

    Fix interpolation issue

--- recon_ao/projectVariants/aoStimReconRunMany_small_quads_chr.m
+++ recon_ao/projectVariants/aoStimReconRunMany_small_quads_chr.m
@@ -94,7 +94,7 @@ prBase.wls = (400:1:700)';
 % want it to be relatively limited for the sake of speed. Of note,
 % regionList options include: center, nearSurround, distantSurround,
 % multiple, global
-prBase.stimSizeDegsList = [2 3]/60;%[2 3.5 10] / 60;
+prBase.stimSizeDegsList = [2 3.5]/60;%[2 3.5 10] / 60;
 prBase.focalRegionList = ["center"];
 prBase.focalPropLList = [0.0 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95 1.0];
 prBase.focalVariantList = [1 2 3 4];
